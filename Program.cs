116333a0509bb2850ac7e26988706346cc321aa2
Dhanya S V <dhanyavirupaksha@gmail.com>
2020-10-21 20:43:11 +0530

    [Dhanya]Add . Added Method To Check Equality Of Lines

--- Program.cs
+++ Program.cs
@@ -4,13 +4,23 @@ namespace LineComparisonComputation
 {
     class Program
     {
+        public static double line1 = 0, line2 = 0;
         static double LengthOfLine(int x1, int y1, int x2, int y2)
         {
             return Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
         }
+        static void EqualityOfLines(double line1, double line2)
+        {
+            if (line1 == line2)
+                Console.WriteLine("The two lines are equal");
+            else
+                Console.WriteLine("not Equal");
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine(LengthOfLine(2, 3, 3, 4));
+            line1 = LengthOfLine(2, 3, 3, 2);
+            line2 = LengthOfLine(2, 3, 3, 2);
+            EqualityOfLines(line1, line2);
         }
     }
 }
