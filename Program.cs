bed819ad26574e9750ba42def264b982ba98387e
Dhanya S V <dhanyavirupaksha@gmail.com>
2020-10-21 21:00:55 +0530

    [Dhanya]Add . Added Method To Compare Two Lines Using CompareTo

--- Program.cs
+++ Program.cs
@@ -9,18 +9,20 @@ namespace LineComparisonComputation
         {
             return Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
         }
-        static void EqualityOfLines(double line1, double line2)
+        static void CompareTwoLines(double line1, double line2)
         {
-            if (line1 == line2)
+            if (line1.CompareTo(line2) == 0)
                 Console.WriteLine("The two lines are equal");
-            else
-                Console.WriteLine("not Equal");
+            if(line1.CompareTo(line2) > 0)
+                Console.WriteLine("The line1 is greater than line2");
+            if (line1.CompareTo(line2) < 0)
+                Console.WriteLine("The line1 is less than line2");
         }
         static void Main(string[] args)
         {
             line1 = LengthOfLine(2, 3, 3, 2);
-            line2 = LengthOfLine(2, 3, 3, 2);
-            EqualityOfLines(line1, line2);
+            line2 = LengthOfLine(2, 3, 4, 2);
+            CompareTwoLines(line1, line2);
         }
     }
 }
